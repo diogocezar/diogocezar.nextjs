eeab00fad007c0cf09ce9e387984268d39613582
Diogo Cezar <diogo@diogocezar.com>
2021-12-17 03:19:58 -0300

    chore: Adjusting image responsive

--- objects/Images/styles.js
+++ objects/Images/styles.js
@@ -3,14 +3,34 @@ import { devices } from '@/styles/devices'
 
 const StyledImage = styled.img`
   border-radius: 50%;
-  width: 70%;
+  width: 85%;
   margin: 0 auto;
   display: block;
   opacity: 0.9;
-  @media ${devices.laptop} {
+  @media ${devices.desktopLarge} {
+    width: 85%;
+    margin-top: 0;
+  }
+  @media ${devices.desktop} {
+    width: 100%;
+    margin-top: 0;
+  }
+  @media ${devices.laptopLarge} {
+    width: 50%;
     margin-top: 0;
     margin-bottom: var(--size-s3);
   }
+  @media ${devices.laptop} {
+    width: 50%;
+    margin-top: 0;
+    margin-bottom: var(--size-s3);
+  }
+  @media ${devices.mobileLarge} {
+    width: 50%;
+    margin-top: 0;
+    margin-bottom: var(--size-s3);
+  }
+
 `
 
 const StyledImageItemColumn = styled.img`
