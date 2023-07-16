d6271abe5deda20b5eb09776006c72545e3d8e78
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2023-07-16 11:20:07 -0300

    feat: add google analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,22 @@ import Content from "@/components/Pages/Index/Content";
 import Conclusion from "@/components/Pages/Index/Conclusion";
 import FooterMain from "@/components/Pages/Footer";
 
+import Script from "next/script";
+
 const Index = ({ index, footer, home }) => {
   const { title, subtitle, biography, citation, citationAuthor } = index.data;
   return (
     <Page color="purple" paddingBottom="0">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-DQS0HM726E" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DQS0HM726E');
+        `}
+      </Script>
       <Wrapper>
         <Introduction title={title} subtitle={subtitle} biography={biography} />
         <Content />
