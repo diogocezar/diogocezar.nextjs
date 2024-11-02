40d819b3cd2ceec6a798aad74b0481dc35a50674
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2024-11-02 16:35:40 -0300

    feat: fix

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,16 +1,16 @@
-import { Page, Wrapper } from "@/components/Containers";
+// import { Page, Wrapper } from "@/components/Containers";
 
-import ProudToBeenPart from "@/components/Pages/Portfolio/ProudToBeenPart";
-import MainCustomers from "@/components/Pages/Portfolio/MainCustomers";
-import Musical from "@/components/Pages/Portfolio/Musical";
-import Agency from "@/components/Pages/Portfolio/Agency";
+// import ProudToBeenPart from "@/components/Pages/Portfolio/ProudToBeenPart";
+// import MainCustomers from "@/components/Pages/Portfolio/MainCustomers";
+// import Musical from "@/components/Pages/Portfolio/Musical";
+// import Agency from "@/components/Pages/Portfolio/Agency";
 
-import FooterMain from "@/components/Pages/Footer";
+// import FooterMain from "@/components/Pages/Footer";
 
 const Portfolio = () => {
   return (
     <>
-      <Page color="purple-dark">
+      {/* <Page color="purple-dark">
         <Wrapper>
           <ProudToBeenPart />
           <MainCustomers />
@@ -18,7 +18,7 @@ const Portfolio = () => {
           <Agency />
         </Wrapper>
       </Page>
-      <FooterMain />
+      <FooterMain /> */}
     </>
   );
 };
